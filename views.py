b160c30029c2b280ccbffb7116aad4699a4f530f
Carlos Cedeno <carlosantoniocedeno@gmail.com>
2016-04-02 04:43:10 +0000

    created landing page template. updated views.

--- views.py
+++ views.py
@@ -3,4 +3,4 @@ from django.http import HttpResponse
 
 # Create your views here.
 def index(request):
-  return HttpResponse("Test String!")
+    return render(request, "main.html", {'testvar': "Test STring 2!"})
