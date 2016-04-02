65e97ac1cac4118bbe83af93762a450cf3d120b8
Carlos Cedeno <carlosantoniocedeno@gmail.com>
2016-04-02 06:04:07 +0000

    Added login functionality to views

--- views.py
+++ views.py
@@ -1,8 +1,16 @@
 from django.shortcuts import render
 from django.http import HttpResponse
 from models import BlogArticle
+from django.contrib.auth import authenticate, login
 
 # Create your views here.
 def index(request):
     blogs = BlogArticle.objects.all()
-    return render(request, "main.html", {'testvar': "Test STring 2!", 'blogs': blogs})
+    if request.method == 'POST':
+      usrname = request.POST['username']
+      pwd = request.POST['password']
+      user = authenticate(username = usrname, password = pwd)
+      if user is not None:
+        login(request, user)
+        return render(request, "main.html", {'testvar': "Test STring 2!", 'blogs': blogs, 'user': user})   
+    return render(request, "main.html", {'testvar': "Test STring 2!", 'blogs': blogs, 'user': None})
