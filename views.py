f224be3a432ecb8237e0abcc1607416cfa2fcbc0
Carlos Cedeno <carlosantoniocedeno@gmail.com>
2016-04-02 05:52:35 +0000

    Updated views so that blogs are visible.

--- views.py
+++ views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from models import BlogArticle
 
 # Create your views here.
 def index(request):
-
-    return render(request, "main.html", {'testvar': "Test STring 2!"})
+    blogs = BlogArticle.objects.all()
+    return render(request, "main.html", {'testvar': "Test STring 2!", 'blogs': blogs})
